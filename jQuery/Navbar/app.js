849edd85d2d34bb18042b349944917944c72a654
minsk0105 <cminseog7065@gmail.com>
2025-04-15 17:40:38 +0900

    Navbar Jquery Transform

---
+++ jQuery/Navbar/app.js
@@ -0,0 +1,16 @@
+// const navToggle = document.getElementById('bar');
+// const navHeader = document.querySelector('header');
+// const nav = navHeader.querySelector('ul');
+
+// navToggle.addEventListener('click', () => {
+//     if (nav.classList.contains("mobile")) {
+//         navHeader.classList.toggle("show");
+//     }
+// });
+
+$('#bar'). on ('click', () => {
+    if ($('nav > ul').hasClass('mobile')) {
+        // addClass, removeClass, toggleClass
+        $('header').toggleClass("show");
+    }
+});
